831cdfe4ca5844df3b72a4e5c8ca041ec38ad08e
mhee167 <mhee116677@gmail.com>
2024-10-12 18:48:38 +0900

    feat : 프로필 수정 기능 추가

---
+++ Downloads/spring-security적용된프론트엔드/DamDda_FE-origin-feature-ju/src/UserContext.js
@@ -0,0 +1,45 @@
+import React, { createContext, useState, useContext, useEffect } from "react";
+import Cookies from "js-cookie";
+const UserContext = createContext();
+
+export const UserProvider = ({ children }) => {
+  const [user, setUser] = useState(() => {
+    // localStorage에서 사용자 정보를 가져옵니다.
+    const savedUser = localStorage.getItem("user");
+    // return savedUser ? JSON.parse(savedUser) : null;
+    return savedUser ? JSON.parse(savedUser) : { key: 0 };
+  });
+
+  const [isLogin, setIsLogin] = useState(0);
+
+  useEffect(() => {
+    // user 상태가 변경될 때마다 localStorage에 저장합니다.
+    if (user) {
+      localStorage.setItem("user", JSON.stringify(user));
+    } else {
+      localStorage.removeItem("user");
+    }
+  }, [user]);
+
+  const login = (userData) => {
+    setUser(userData);
+    setIsLogin(true);
+  };
+
+  const logout = () => {
+    setUser({ key: 0 });
+    // localStorage에서 사용자 정보를 삭제합니다.
+    localStorage.removeItem("user");
+    Cookies.remove("accessToken");
+
+    setIsLogin(false);
+  };
+
+  return (
+    <UserContext.Provider value={{ user, login, logout, isLogin, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export const useUser = () => useContext(UserContext);
